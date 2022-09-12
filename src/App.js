2cf0d381bbbf2f8512229bf1ee092cf8ade21d4b
“Ryan” <pagsmanagement@gmail.com>
2022-09-12 10:23:16 -0600

    NavBar Init

--- src/App.js
+++ src/App.js
@@ -3,7 +3,16 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+      <div className="NavBar">
+        <div className="LocationTitle">
+          <div className="LeftSideNavBar">
+            <h1>London</h1>
+          </div>
+          <div className="RightSideNavBar">
 
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
