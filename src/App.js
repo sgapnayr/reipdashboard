d74fff3da0fe4caec50e1ff90f46eac7216c6998
“Ryan” <pagsmanagement@gmail.com>
2022-09-12 10:29:32 -0600

    Api Init

--- src/App.js
+++ src/App.js
@@ -1,18 +1,36 @@
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
 import './App.css';
 
 function App() {
+  const [apiData, setApiData] = useState('')
+
+  const url = ``
+
+  function getData() {
+    axios.get(url).then(res => setApiData(res.data)).catch(err => console.log(err))
+  }
+
+  useEffect(() => {
+    getData()
+  }, [])
+
   return (
     <div className="App">
+
       <div className="NavBar">
         <div className="LocationTitle">
           <div className="LeftSideNavBar">
             <h1>London</h1>
           </div>
           <div className="RightSideNavBar">
-
           </div>
         </div>
       </div>
+
+      <form action="">
+        <input type="text" />
+      </form>
     </div>
   );
 }
