c1bef2888bfa174082a52762cf9a5aad3f236937
“Ryan” <pagsmanagement@gmail.com>
2022-09-13 22:17:27 -0600

    Charts and Visibility Function

--- src/App.js
+++ src/App.js
@@ -2,9 +2,13 @@ import React, { useState, useEffect } from 'react'
 import axios from 'axios'
 import './App.css';
 import NavBar from './compoonents/NavBar/NavBar';
+import ShowChart from './compoonents/DummyChart/Chart';
+import BarChart from './compoonents/DummyChart/Bar';
+import { faSearch } from '@fortawesome/free-solid-svg-icons'
 
 function App() {
   const [apiData, setApiData] = useState([])
+  const [visibility, setVisibility] = useState(false)
 
   const url = `https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false`
 
@@ -14,22 +18,28 @@ function App() {
     axios.get(url).then(res => setApiData(res.data)).catch(err => console.log(err))
   }
 
+  const handleClick = () => {
+    setVisibility(!visibility)
+  }
+
   useEffect(() => {
     GetData()
   }, [])
-
   return (
     <div className="App">
       <NavBar />
 
       <div className="Carousel">
-        Ticker Info
+        ↑CPI: 2.3%
       </div>
 
       <div className="DataContainers">
         <div className="DataContainer FirstRow">
           <div className="DataDiv LongRow">
-            <div className="Chart">Chart</div>
+            <p>Market Trend</p>
+            <div className="Chart">
+              <ShowChart />
+            </div>
           </div>
           <div className="DataDiv">
             <div className="Rates">
@@ -39,17 +49,30 @@ function App() {
           </div>
         </div>
 
-        <div className="DataContainer SecondRow">
-          To Be Determined...
+        <button onClick={handleClick}>{visibility ? 'Visible' : 'Visibility-None'}</button>
+        <div className={visibility ? 'DataContainer SecondRow' : 'None'}>
+          <div className='DataDiv'>
+            More Information... Blah Blah
+          </div>
         </div>
         <div className="DataContainer ThirdRow">
-          <div className="DataDiv">Mortgage</div>
-          <div className="DataDiv">Graph</div>
-          <div className="DataDiv">Graph</div>
+          <div className="DataDiv">
+            <div className="Rates2">
+              <p>CPI</p>
+              <h1>6.3%</h1>
+            </div>
+          </div>
+          <div className="DataDiv">
+            <p>Population</p>
+            <div className="Chart">
+              <BarChart />
+            </div>
+          </div>
+          <div className="DataDiv">News</div>
         </div>
       </div>
 
-    </div>
+    </div >
   );
 }
 
---
+++ src/compoonents/DummyChart/Chart.js
@@ -0,0 +1,93 @@
+import React, { useEffect, useState } from 'react'
+import { Chart as Chartjs } from 'chart.js/auto'
+import { Line } from 'react-chartjs-2'
+import axios from 'axios'
+
+export default function ShowChart() {
+    const [apiData, setApiData] = useState([])
+    const url = `https://api.coingecko.com/api/v3/coins/bitcoin/market_chart?vs_currency=usd&days=30`
+    console.log(url)
+
+    async function GetData() {
+        await axios.get(url).then(res => setApiData(res.data)).catch(err => console.log(err))
+    }
+
+    useEffect(() => {
+        GetData()
+    }, [])
+
+    const priceData = apiData?.prices?.map(el => el[1])
+    const volumeLabels = apiData?.total_volumes?.map(el => new Date(el[0]).getDate().toString()).map(el => el.length === 1 ? `0${el}` : el)
+
+    const data = {
+        labels: volumeLabels?.slice(0, 20),
+        datasets: [
+            {
+                label: '',
+                data: priceData,
+                backgroundColor: [
+                    'rgba(255, 99, 132, 0.2)',
+                    'rgba(54, 162, 235, 0.2)',
+                    'rgba(255, 206, 86, 0.2)',
+                    'rgba(75, 192, 192, 0.2)',
+                    'rgba(153, 102, 255, 0.2)',
+                    'rgba(255, 159, 64, 0.2)'
+                ],
+                lineTension: .4,
+                borderColor: [
+                    'rgba(25, 99, 132, 1)',
+                    'rgba(54, 162, 235, 1)',
+                    'rgba(255, 206, 86, 1)',
+                    'rgba(75, 192, 192, 1)',
+                    'rgba(153, 102, 255, 1)',
+                    'rgba(255, 159, 64, 1)'
+                ],
+                borderWidth: 3,
+                fontStyle: "bold",
+                fill: false,
+                data: priceData?.slice(0, 20),
+            }]
+    }
+
+    const options = {
+        plugins: {
+            legend: {
+                display: false,
+            },
+            layout: {
+                padding: 1,
+            },
+            responsive: true,
+            maintainAspectRatio: false,
+        },
+        elements: {
+            point: {
+                radius: 2,
+            },
+        },
+        scales: {
+            y: {
+                grid: {
+                    display: false,
+                    drawTicks: false,
+                },
+                ticks: {
+                    display: false,
+                },
+            },
+            x: {
+                grid: {
+                    display: false,
+                },
+                ticks: {
+                    display: false,
+                },
+            },
+        },
+    };
+    return (
+        <>
+            <Line data={data} options={options} />
+        </>
+    )
+}
